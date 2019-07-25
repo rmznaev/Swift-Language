36b2be7a94d8c34b016f09e6fc4120395ca3f184
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-07-25 18:14:47 +0400

    Optionals updated

--- Swift_Advanced.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -2,6 +2,28 @@
 
 import Foundation
 
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveyAnswer: String?
+
+if convertedNumber != nil {
+    print("convertedNumber contains some integer value of \(convertedNumber!).")
+}
+
+if let actualNumber = Int(possibleNumber) {
+    print("The string \" \(possibleNumber) \" has an integer value of \(actualNumber)")
+} else {
+    print("The string \"\(possibleNumber)\" could not be converted to an integer")
+}
+
+if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100 {
+    print("\(firstNumber) < \(secondNumber) < 100")
+}
+
 
 
 //: [Next](@next)
