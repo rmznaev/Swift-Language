76cccb98f4ab4bf76e3cdec849e98a0f62231f71
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-08-13 23:05:39 +0400

    Optionals - UPDATE -

--- Swift_Advanced.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -25,5 +25,40 @@ if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secon
 }
 
 
+// Successful
+let myProfileImageURL: String? = "https//facebook.com/rmznabb"
+
+let myName: String? = nil
+let yourName: String? = "Rmzn"
+
+//print(myName!) // nil -> ERROR (YOU CANNOT FORCE UNWRAP nil VALUE )
+print(yourName!) // Optional("Rmzn")
+
+let rmznAge: Int? = 24
+let shlAge: Int? = 23
+let nctAge: Int? = 27
+
+let newRmznAge = rmznAge!
+print(newRmznAge) // normal type
+
+let newShlAge = shlAge!
+print(newShlAge) // normal type
+
+// Problem
+var image: String? = "rmzn.jpg" // CANNOT BE nil VALUE -> ERROR
+var hahaha: String? = image
+
+print(hahaha!)
+
+
+// IMPLICIT UNWRAPPING
+
+let imageFromFaceBook: String? = nil // "Rmzn's Photo"
+
+if let normalImage = imageFromFaceBook {
+    print(normalImage)
+} else {
+    print("There is no image!")
+}
 
 //: [Next](@next)
