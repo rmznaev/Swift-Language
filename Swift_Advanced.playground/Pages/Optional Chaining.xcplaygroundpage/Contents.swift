de0b0ecfb8adefa7e6f24c407510e3bef9bc3f73
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-08-27 15:56:33 +0400

    Unfinished Optional Chaining code

--- Swift_Advanced.playground/Pages/Optional Chaining.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Optional Chaining.xcplaygroundpage/Contents.swift
@@ -46,9 +46,70 @@ class Person {
     
 }
 
+class Room {
+    
+    let name: String
+    init(name: String) { self.name = name }
+    
+}
+
 class Residence {
     
-    var numberOfRooms = 4
+    var rooms = [Room]()
+    
+    var numberOfRooms: Int {
+        
+        return rooms.count
+        
+    }
+    
+    subscript(i: Int) -> Room {
+        
+        get {
+            
+            return rooms[i]
+            
+        } set {
+            
+            rooms[i] = newValue
+            
+        }
+        
+    }
+    
+    func printNumberOfRows() {
+        
+        print("The number of rooms is \(numberOfRooms)")
+        
+    }
+    
+    var address: Address?
+    
+}
+
+class Address {
+    
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        
+        if let buildingNumber = buildingNumber, let street = street {
+            
+            return "\(buildingNumber) \(street)"
+            
+        } else if buildingName != nil {
+            
+            return buildingName
+            
+        } else {
+            
+            return nil
+            
+        }
+        
+    }
     
 }
 
