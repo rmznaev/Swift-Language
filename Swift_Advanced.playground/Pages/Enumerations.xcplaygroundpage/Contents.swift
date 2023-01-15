f70c78c55eb48840eece74aaa2b84cc222723d22
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-15 20:08:34 +0100

    added new Enumeration codes from another sources

--- Swift_Advanced.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
@@ -116,5 +116,186 @@ if let somePlanet = PlanetAgain(rawValue: positionToFind) {
 }
 
 
+// - Swift Apprentice book
+
+let months = [
+    "January", "February", "March", "April", "May",
+    "June", "July", "August", "September", "October",
+    "November", "December"
+]
+
+func semester(for month: String) -> String {
+    switch month {
+    case "August", "September", "October", "November", "December":
+        return "Autumn"
+    case "January", "February", "March", "April", "May":
+        return "Spring"
+    default:
+        return "Not in the school year"
+    }
+}
+
+semester(for: "December")
+
+enum Month {
+  case january, february, march,
+       april, may, june, july, august,
+       september, october, november, december
+}
+
+func semester(for month: Month) -> String {
+    switch month {
+    case .august, .september, .october, .november, .december:
+        return "Autumn"
+    case .january, .february, .march, .april, .may:
+        return "Spring"
+    case .june, .july:
+        return "Summer"
+    }
+}
+
+var month = Month.april
+semester(for: month) // "Spring"
+
+month = .september
+semester(for: month) // "Autumn"
+
+
+// Raw values
+
+enum RawMonth: Int {
+    case january = 1, february, march,
+         april, may, june, july, august,
+         september, october, november, december
+}
+
+func monthsUntilWinterBreak(from month: RawMonth) -> Int {
+    RawMonth.december.rawValue - month.rawValue
+}
+
+let fifthMonth = RawMonth.may // RawMonth(rawValue: 5)!
+monthsUntilWinterBreak(from: fifthMonth)
+
+
+// String raw values
+
+enum Icon: String {
+    case music
+    case sports
+    case weather
+    
+    var filename: String {
+        "\(rawValue).png"
+    }
+}
+
+let icon = Icon.weather
+print(icon.filename) // weather.png
+
+
+// Unordered raw values
+
+enum Coin: Int {
+    case penny = 1
+    case nickel = 5
+    case dime = 10
+    case quarter = 25
+}
+
+let coin = Coin.quarter
+coin.rawValue // 25
+
+
+// Associated values
+
+var balance = 100
+
+enum WithdrawalResult {
+    case success(newBalance: Int)
+    case error(message: String)
+}
+
+func withdraw(amount: Int) -> WithdrawalResult {
+    if amount <= balance {
+        balance -= amount
+        return .success(newBalance: balance)
+    } else {
+        return .error(message: "Not enough money!")
+    }
+}
+
+let result = withdraw(amount: 99)
+
+switch result {
+case .success(let newBalance):
+    print("Your current balance is: \(newBalance)")
+case .error(let message):
+    print(message)
+}
+
+
+enum HTTPMethod {
+    case get
+    case post(body: String)
+}
+
+let request = HTTPMethod.post(body: "Hi there!")
+
+guard case .post(let body) = request else {
+    fatalError("No message was posted")
+}
+print(body)
+
+
+// Enumeration as a state machine
+
+enum TrafficLight {
+    case red, yellow, green
+}
+
+let trafficLight = TrafficLight.red
+
+
+// Iterating through all cases
+
+enum Pet: CaseIterable {
+    case cat, dog, bird, turtle, fish, hamster
+}
+
+for pet in Pet.allCases {
+    print(pet)
+}
+
+
+// Enumerations without any cases
+
+enum Math {
+    static func factorial(of number: Int) -> Int {
+        (1...number).reduce(1, *)
+    }
+}
+
+let factorial = Math.factorial(of: 6) // 720
+print(factorial)
+
+
+// Optionals
+
+var age: Int?
+age = 17
+age = .some(7)
+age = .none
+
+switch age {
+case .some(let value):
+    print("Got a value: \(value)")
+case .none:
+    print("No value")
+}
+
+let optionalNil: Int? = .none
+optionalNil == nil    // true
+optionalNil == .none  // true
+
 
 //: [Next](@next)
