379b3ccef1922d21e85e28b7b140261477fcb8da
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-17 20:58:00 +0100

    created new playground file for Structures

---
+++ Swift_Advanced.playground/Pages/Structures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// - Structures
+/// Structures are types that can store named properties and define actions and behaviors.
+
+
+
+//: [Next](@next)
