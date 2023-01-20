6e8ddfdd07527f7b6d37086e1bbc81de561b1428
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-20 21:37:50 +0100

    added property wrappers code samples

--- Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -111,5 +111,176 @@ stepCounter.totalSteps = 896
 // Added 536 steps
 
 
+// Property Wrappers
+@propertyWrapper
+struct TwelveOrLess {
+    private var number = 0
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, 12) }
+    }
+}
+
+struct SmallRectangle {
+    @TwelveOrLess var width: Int
+    @TwelveOrLess var height: Int
+}
+
+var rectangle = SmallRectangle()
+
+print(rectangle.height)
+// Prints "0"
+
+rectangle.height = 10
+print(rectangle.height)
+// Prints "10"
+
+rectangle.height = 24
+print(rectangle.height)
+// Prints "12"
+
+/* For example, here’s a version of SmallRectangle from the previous code
+ listing that wraps its properties in the TwelveOrLess structure explicitly,
+ instead of writing @TwelveOrLess as an attribute: */
+struct SmallRectangleAgain {
+    private var _height = TwelveOrLess()
+    private var _width = TwelveOrLess()
+    var height: Int {
+        get { return _height.wrappedValue }
+        set { _height.wrappedValue = newValue }
+    }
+    var width: Int {
+        get { return _width.wrappedValue }
+        set { _width.wrappedValue = newValue }
+    }
+}
+
+
+
+@propertyWrapper
+struct SmallNumber {
+    private var maximum: Int
+    private var number: Int
+
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, maximum) }
+    }
+
+    init() {
+        maximum = 12
+        number = 0
+    }
+    init(wrappedValue: Int) {
+        maximum = 12
+        number = min(wrappedValue, maximum)
+    }
+    init(wrappedValue: Int, maximum: Int) {
+        self.maximum = maximum
+        number = min(wrappedValue, maximum)
+    }
+}
+
+struct ZeroRectangle {
+    @SmallNumber var height: Int
+    @SmallNumber var width: Int
+}
+
+var zeroRectangle = ZeroRectangle()
+print(zeroRectangle.height, zeroRectangle.width)
+// Prints "0 0"
+
+struct UnitRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber var width: Int = 1
+}
+
+var unitRectangle = UnitRectangle()
+print(unitRectangle.height, unitRectangle.width)
+// Prints "1 1"
+
+struct NarrowRectangle {
+    @SmallNumber(wrappedValue: 2, maximum: 5) var height: Int
+    @SmallNumber(wrappedValue: 3, maximum: 4) var width: Int
+}
+
+var narrowRectangle = NarrowRectangle()
+print(narrowRectangle.height, narrowRectangle.width)
+// Prints "2 3"
+
+struct MixedRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber(maximum: 9) var width: Int = 2
+}
+
+var mixedRectangle = MixedRectangle()
+print(mixedRectangle.height)
+// Prints "1"
+
+mixedRectangle.height = 20
+print(mixedRectangle.height)
+// Prints "12"
+
+
+// Projecting a Value From a Property Wrapper
+@propertyWrapper
+struct ProjectingSmallNumber {
+    private var number: Int
+    private(set) var projectedValue: Bool
+
+    var wrappedValue: Int {
+        get { return number }
+        set {
+            if newValue > 12 {
+                number = 12
+                projectedValue = true
+            } else {
+                number = newValue
+                projectedValue = false
+            }
+        }
+    }
+
+    init() {
+        self.number = 0
+        self.projectedValue = false
+    }
+}
+struct SomeStructure {
+    @ProjectingSmallNumber var someNumber: Int
+}
+var someStructure = SomeStructure()
+
+someStructure.someNumber = 4
+print(someStructure.$someNumber)
+// Prints "false"
+
+someStructure.someNumber = 55
+print(someStructure.$someNumber)
+// Prints "true"
+
+
+enum RectangleSize {
+    case small
+    case large
+}
+
+struct SizedRectangle {
+    @ProjectingSmallNumber var height: Int
+    @ProjectingSmallNumber var width: Int
+
+    mutating func resize(to size: RectangleSize) -> Bool {
+        switch size {
+        case .small:
+            height = 10
+            width = 20
+        case .large:
+            height = 100
+            width = 100
+        }
+
+        return $height || $width
+    }
+}
 
 //: [Next](@next)
