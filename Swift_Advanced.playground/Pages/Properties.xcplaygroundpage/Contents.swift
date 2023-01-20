efdf16f78dc7f913186b8650e5e4a77038d147d4
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-20 19:26:03 +0100

    added codes for Stored properties

--- Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -4,7 +4,32 @@ import Foundation
 
 // Properties - Swift Language Guide
 
+// Stored properties
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+/* rangeOfFourItems.firstValue = 7 */ /// this will report an error, even though firstValue is a variable property
 
 
+// Lazy stored properties
+class DataImporter {
+    var fileName = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+print(manager.importer.fileName)
 
 //: [Next](@next)
