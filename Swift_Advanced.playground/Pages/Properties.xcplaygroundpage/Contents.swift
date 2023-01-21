e5df97ec1b13c099a6b8a3fa53c3a3c645d69d12
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-21 08:47:55 +0100

    added all codes about properties for now

--- Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -311,12 +311,8 @@ struct TV {
     var height: Double
     var width: Double
 
-    // 1
     var diagonal: Int {
-        // 2
-        let result = (height * height + width * width).squareRoot().rounded()
-        // 3
-        return Int(result)
+        return Int((height * height + width * width).squareRoot().rounded())
     }
 }
 
@@ -326,5 +322,99 @@ print(tv.diagonal) // 110
 tv.width = tv.height
 print(tv.diagonal) // 76
 
+
+// Getter and setter
+struct Television {
+    var height: Double
+    var width: Double
+    
+    var diagonal: Int {
+      get {
+        let result = (height * height +
+          width * width).squareRoot().rounded()
+        return Int(result)
+      }
+      set {
+        let ratioWidth = 16.0
+        let ratioHeight = 9.0
+        let ratioDiagonal = (ratioWidth * ratioWidth +
+          ratioHeight * ratioHeight).squareRoot()
+        height = Double(newValue) * ratioHeight / ratioDiagonal
+        width = height * ratioWidth / ratioHeight
+      }
+    }
+}
+
+var television = Television(height: 53.93, width: 95.87)
+television.diagonal = 70
+print(television.height) // 34.32...
+print(television.width) // 61.01...
+
+
+// Type properties
+struct Level {
+    static var highestLevel = 1
+
+    let id: Int
+    var boss: String
+    var unlocked: Bool {
+        didSet {
+            if unlocked && id > Self.highestLevel {
+                Self.highestLevel = id
+            }
+        }
+    }
+}
+
+let level1 = Level(id: 1, boss: "Chameleon", unlocked: true)
+let level2 = Level(id: 2, boss: "Squid", unlocked: false)
+let level3 = Level(id: 3, boss: "Chupacabra", unlocked: false)
+let level4 = Level(id: 4, boss: "Yeti", unlocked: false)
+
+
+// Limiting a variable
+struct LightBulb {
+    static let maxCurrent = 40
+    var current = 0 {
+        didSet {
+            if current > LightBulb.maxCurrent {
+                print("""
+                    Current is too high,
+                    falling back to previous setting.
+                    """)
+                current = oldValue
+            }
+        }
+    }
+}
+
+var light = LightBulb()
+light.current = 50
+light.current // 0
+light.current = 40
+light.current // 40
+
+
+// Lazy properties
+struct Circle {
+    lazy var pi = {
+        ((4.0 * atan(1.0 / 5.0)) - atan(1.0 / 239.0)) * 4.0
+    }()
+
+    var radius = 0.0
+
+    var circumference: Double {
+        mutating get {
+            pi * radius * 2
+        }
+    }
+    
+    init(radius: Double) {
+        self.radius = radius
+    }
+}
+
+var circle = Circle(radius: 5) /// got a circle, pi has not been run
+circle.circumference
 
 //: [Next](@next)
