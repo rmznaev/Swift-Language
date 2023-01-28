225a413c309c0ceec610f1bf5a7a266823b89615
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-28 21:57:40 +0100

    cleaned unnecessary empty lines from Optional Chaining file

--- Swift_Advanced.playground/Pages/Optional Chaining.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Optional Chaining.xcplaygroundpage/Contents.swift
@@ -41,100 +41,62 @@ if let residentName = nycApartment.human?.name {
 // Swift Book reference
 
 class Person {
-    
     var residence: Residence?
-    
 }
 
 class Room {
-    
     let name: String
     init(name: String) { self.name = name }
-    
 }
 
 class Residence {
-    
+    var address: Address?
     var rooms = [Room]()
-    
     var numberOfRooms: Int {
-        
         return rooms.count
-        
     }
     
     subscript(i: Int) -> Room {
-        
         get {
-            
             return rooms[i]
-            
         } set {
-            
             rooms[i] = newValue
-            
         }
-        
     }
     
     func printNumberOfRows() {
-        
         print("The number of rooms is \(numberOfRooms)")
-        
     }
-    
-    var address: Address?
-    
 }
 
 class Address {
-    
     var buildingName: String?
     var buildingNumber: String?
     var street: String?
     
     func buildingIdentifier() -> String? {
-        
         if let buildingNumber = buildingNumber, let street = street {
-            
             return "\(buildingNumber) \(street)"
-            
         } else if buildingName != nil {
-            
             return buildingName
-            
         } else {
-            
             return nil
-            
         }
-        
     }
-    
 }
 
 let asif = Person()
-
 if let roomCount = asif.residence?.numberOfRooms {
-    
     print("Asif's residence has \(roomCount) room(s).")
-    
 } else {
-    
     print("Unable to retrieve number of rooms")
-    
 }
 
 asif.residence = Residence()
-
 if let roomCount = asif.residence?.numberOfRooms {
-    
     print("John's residence has \(roomCount) room(s).")
-    
 } else {
-    
     print("Unable to retrieve number of rooms")
-    
 }
 
 //: [Next](@next)
