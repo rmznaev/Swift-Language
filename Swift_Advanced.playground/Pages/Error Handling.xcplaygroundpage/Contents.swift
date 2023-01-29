c32957db9353a6b5d75fab1cdcb977037b5bbf77
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-29 13:46:46 +0100

    extended Error Handling codes with additional valuable contents

--- Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -89,4 +89,75 @@ let myNewCourse = try! Course(name: "Swift with Rmzn")
 
 // When it comes to the real programming, to use the simple try is the better approach
 
+
+
+// - Swift Language Guide
+
+// Representing and Throwing Errors
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+throw VendingMachineError.insufficientFunds(coinsNeeded: 7)
+
+
+// Propagating Errors Using Throwing Functions
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    let favoriteSnacks = [
+        "Alice": "Chips",
+        "Bob": "Licorice",
+        "Eve": "Pretzels",
+    ]
+    var coinsDeposited = 0
+    
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+
+        guard item.count > 0 else  {
+            throw VendingMachineError.outOfStock
+        }
+
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(
+                coinsNeeded: item.price - coinsDeposited
+            )
+        }
+
+        coinsDeposited -= item.price
+
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        print("Dispensing \(name)")
+    }
+
+    func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+        let snackName = favoriteSnacks[person] ?? "Candy Bar"
+        try vendingMachine.vend(itemNamed: snackName)
+    }
+}
+
+struct PurchasedSnack {
+    let name: String
+
+    init(name: String, vendingMachine: VendingMachine) throws {
+        try vendingMachine.vend(itemNamed: name)
+        self.name = name
+    }
+}
+
 //: [Next](@next)
