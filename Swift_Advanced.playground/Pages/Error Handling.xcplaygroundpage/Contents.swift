7a586ad77fb431cb39ba94b506d508b803b94810
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-29 13:58:19 +0100

    added more codes for Error Handling

--- Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -115,11 +115,6 @@ class VendingMachine {
         "Chips": Item(price: 10, count: 4),
         "Pretzels": Item(price: 7, count: 11)
     ]
-    let favoriteSnacks = [
-        "Alice": "Chips",
-        "Bob": "Licorice",
-        "Eve": "Pretzels",
-    ]
     var coinsDeposited = 0
     
     func vend(itemNamed name: String) throws {
@@ -144,11 +139,17 @@ class VendingMachine {
         inventory[name] = newItem
         print("Dispensing \(name)")
     }
+}
 
-    func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
-        let snackName = favoriteSnacks[person] ?? "Candy Bar"
-        try vendingMachine.vend(itemNamed: snackName)
-    }
+let favoriteSnacks = [
+    "Alice": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels",
+]
+
+func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
 }
 
 struct PurchasedSnack {
@@ -157,6 +158,53 @@ struct PurchasedSnack {
     init(name: String, vendingMachine: VendingMachine) throws {
         try vendingMachine.vend(itemNamed: name)
         self.name = name
+    }
+}
+
+
+// Handling Errors Using Do-Catch
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+
+do {
+    try buyFavoriteSnack(person: "Amrah", vendingMachine: vendingMachine)
+    print("Success! Yum.")
+} catch VendingMachineError.invalidSelection {
+    print("Invalid selection!")
+} catch VendingMachineError.outOfStock {
+    print("Out of stock!")
+} catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+    print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+} catch {
+    print("Unexpected error: \(error).")
+}
+
+
+func nourish(with item: String) throws {
+    do {
+        try vendingMachine.vend(itemNamed: item)
+    } catch is VendingMachineError {
+        print("Couldn't buy that from the vending machine.")
+    }
+}
+
+do {
+    try nourish(with: "Beet-Flavored Chips")
+} catch {
+    print("Unexpected non-vending-machine-related error: \(error)")
+}
+/// Prints "Couldn't buy that from the vending machine."
+
+
+func eat(item: String) throws {
+    do {
+        try vendingMachine.vend(itemNamed: item)
+    } catch
+        VendingMachineError.invalidSelection,
+        VendingMachineError.insufficientFunds,
+        VendingMachineError.outOfStock
+    {
+        print("Invalid selection, out of stock, or not enough money.")
     }
 }
 
