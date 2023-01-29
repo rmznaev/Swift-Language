9393f86d58ab86a96bf3ee37727c3c568d3bb035
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-29 22:12:43 +0100

    added code samples for throwing and handling errors

--- Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -337,5 +337,109 @@ print("Pet names without optional: \(petNamesWithoutOptional)")
 
 
 
+// Error protocol
+enum BakeryError: Error {
+    case tooFew(numberOnHand: Int)
+    case doNotSell
+    case wrongFlavor
+    case inventory
+    case noPower
+}
+
+class Pastry {
+    let flavor: String
+    var numberOnHand: Int
+
+    init(flavor: String, numberOnHand: Int) {
+        self.flavor = flavor
+        self.numberOnHand = numberOnHand
+    }
+}
+
+// Throwing errors
+class Bakery {
+    var itemsForSale = [
+        "Cookie": Pastry(flavor: "ChocolateChip", numberOnHand: 20),
+        "PopTart": Pastry(flavor: "WildBerry", numberOnHand: 13),
+        "Donut" : Pastry(flavor: "Sprinkles", numberOnHand: 24),
+        "HandPie": Pastry(flavor: "Cherry", numberOnHand: 6)
+    ]
+
+    func open(_ now: Bool = Bool.random()) throws -> Bool {
+        guard now else {
+            throw Bool.random() ? BakeryError.inventory : BakeryError.noPower
+        }
+
+        return now
+    }
+
+    func orderPastry(
+        item: String,
+        amountRequested: Int,
+        flavor: String
+    ) throws -> Int {
+        guard let pastry = itemsForSale[item] else {
+            throw BakeryError.doNotSell
+        }
+
+        guard flavor == pastry.flavor else {
+            throw BakeryError.wrongFlavor
+        }
+
+        guard amountRequested <= pastry.numberOnHand else {
+            throw BakeryError.tooFew(numberOnHand: pastry.numberOnHand)
+        }
+
+        pastry.numberOnHand -= amountRequested
+        return pastry.numberOnHand
+    }
+}
+
+//let bakery = Bakery()
+//bakery.open()
+//bakery.orderPastry(item: "Albatross",
+//                   amountRequested: 1,
+//                   flavor: "AlbatrossFlavor")
+
+// Handling errors
+let bakery = Bakery()
+
+do {
+    try bakery.open()
+    try bakery.orderPastry(item: "Albatross",
+                          amountRequested: 1,
+                          flavor: "AlbatrossFlavor")
+} catch BakeryError.inventory, BakeryError.noPower {
+    print("Sorry, the bakery is now closed.")
+} catch BakeryError.doNotSell {
+    print("Sorry, but we don’t sell this item.")
+} catch BakeryError.wrongFlavor {
+    print("Sorry, but we don’t carry this flavor.")
+} catch BakeryError.tooFew {
+    print("Sorry, we don’t have enough items to fulfill your order.")
+}
+
+// Not looking at the detailed error
+let open = try? bakery.open(false)
+let remaining = try? bakery.orderPastry(item: "Albatross",
+                                        amountRequested: 1,
+                                        flavor: "AlbatrossFlavor")
+
+
+// Stoping your program on an error
+do {
+    try bakery.open(true)
+    try bakery.orderPastry(item: "Cookie",
+                           amountRequested: 1,
+                           flavor: "ChocolateChip")
+} catch {
+    fatalError()
+}
+
+// or..
+try! bakery.open(true)
+try! bakery.orderPastry(item: "Cookie",
+                        amountRequested: 1,
+                        flavor: "ChocolateChip")
 
 //: [Next](@next)
