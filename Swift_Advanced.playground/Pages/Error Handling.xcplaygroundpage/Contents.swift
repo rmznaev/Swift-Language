10c0af6a54e3d6d42e6d5b81737d92903b6b7264
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-01-29 21:48:33 +0100

    added codes for optional chaining into the Error Handling file

--- Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -260,6 +260,81 @@ let tooSmall = PetHouse(squareFeet: 0) /// nil
 let house = PetHouse(squareFeet: 1)    /// Optional(Pethouse)
 
 
+// Optional Chaining
+class Toy {
+    enum Kind {
+        case ball, zombie, bone, mouse
+    }
+
+    enum Sound {
+        case squeak, bell
+    }
+
+    let kind: Kind
+    let color: String
+    var sound: Sound?
+
+    init(kind: Kind, color: String, sound: Sound? = nil) {
+        self.kind = kind
+        self.color = color
+        self.sound = sound
+    }
+}
+
+class Pet {
+    enum Kind {
+        case dog, cat, guineaPig
+    }
+
+    var name: String
+    var kind: Kind
+    var favoriteToy: Toy?
+
+    init(name: String, kind: Kind, favoriteToy: Toy? = nil) {
+        self.name = name
+        self.kind = kind
+        self.favoriteToy = favoriteToy
+    }
+}
+
+class Person {
+    let pet: Pet?
+
+    init(pet: Pet? = nil) {
+        self.pet = pet
+    }
+}
+
+//let delia = Pet(breed: "pug")
+//let olive = Pet()
+
+//let janie = Person(pet: olive)
+/* let dogBreed = janie.pet.breed! */ /// This is bad! Will cause a crash!
+
+//if let dogBreed = janie.pet?.breed {
+//    print("Olive is a \(dogBreed).")
+//} else {
+//    print("Olive’s breed is unknown.")
+//}
+
+let janie = Person(pet: Pet(name: "Delia", kind: .dog,
+                   favoriteToy: Toy(kind: .ball,
+                   color: "Purple", sound: .bell)))
+let tammy = Person(pet: Pet(name: "Evil Cat Overlord",
+                   kind: .cat, favoriteToy: Toy(kind: .mouse,
+                   color: "Orange")))
+let felipe = Person()
+
+
+// Map and compactMap
+let team = [janie, tammy, felipe]
+
+let petNames = team.map { $0.pet?.name }
+print("Pet names: \(petNames)")
+
+let petNamesWithoutOptional = team.compactMap { $0.pet?.name }
+print("Pet names without optional: \(petNamesWithoutOptional)")
+
 
 
 
