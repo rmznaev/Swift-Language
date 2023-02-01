d3d6f8802460e75e599c74c245fbdffa89ba0cf8
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-02-01 16:16:09 +0100

    added all code samples for concurrency

--- Swift_Advanced.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
@@ -216,4 +216,31 @@ extension Playlist: CustomStringConvertible {
     }
 }
 
+
+// Sendable
+final class BasicPlaylist {
+    let title: String
+    let author: String
+
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
+    }
+}
+
+extension BasicPlaylist: Sendable {}
+
+/// Functions and closures can also conform to Sendable
+func execute(task: @escaping @Sendable () -> Void,
+             with priority: TaskPriority? = nil) {
+    Task(priority: priority, operation: task)
+}
+
+@Sendable func showRandomNumber() {
+    let number = Int.random(in: 1...10)
+    print(number)
+}
+
+execute(task: showRandomNumber)
+
 //: [Next](@next)
