5143f5e378ba7a6c4edc3d70cb8a80c58dbcad46
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-02-02 21:44:50 +0100

    added full codes for Extensions

--- Swift_Advanced.playground/Pages/Extension.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Extension.xcplaygroundpage/Contents.swift
@@ -164,4 +164,41 @@ extension Int {
 0746381295[9]
 
 
+// Nested Types
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0 ", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
+// Prints "+ + - 0 - 0 + "
+
+
+
+
 //: [Next](@next)
