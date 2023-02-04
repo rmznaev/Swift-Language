a79e9e75968a349e74b6e7c3e99e1ef238a073d2
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-02-05 00:45:45 +0100

    added last code samples for the Protocols

--- Swift_Advanced.playground/Pages/Protocols SAB.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Protocols SAB.xcplaygroundpage/Contents.swift
@@ -122,4 +122,99 @@ class LightThing: WeightCalculatable {
 }
 
 
+// Extensions & protocol conformance
+protocol Reflective {
+    var typeName: String { get }
+}
+
+extension String: Reflective {
+    var typeName: String {
+        "I'm a String"
+    }
+}
+
+let title = "Swift Apprentice!"
+title.typeName /// "I'm a String"
+
+protocol Wheeled {
+    var numberOfWheels: Int { get }
+    var wheelSize: Double { get set }
+}
+
+class AnotherBike: Wheeled {
+    var peddling = false
+    let numberOfWheels = 2
+    var wheelSize = 16.0
+}
+
+extension AnotherBike: Vehicle {
+    func accelerate() {
+        peddling = true
+    }
+
+    func stop() {
+        peddling = false
+    }
+}
+
+
+// Requiring reference semantics
+protocol Named {
+    var name: String { get set }
+}
+
+class ClassyName: Named {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct StructyName: Named {
+    var name: String
+}
+
+var classyNamed: Named = ClassyName(name: "Classy")
+var classyCopy = classyNamed
+
+classyNamed.name = "Still Classy?"
+classyNamed.name /// Still Classy
+classyCopy.name /// Still Classy
+
+
+var structyNamed: Named = StructyName(name: "Structy")
+var structyCopy = structyNamed
+
+structyNamed.name = "Still Structy?"
+structyNamed.name /// Still Structy?
+structyCopy.name /// "Structy"
+
+
+protocol NamedAgain: AnyObject {
+    var name: String { get set }
+}
+
+
+// Equatable
+class Record: Equatable {
+    var wins: Int
+    var losses: Int
+
+    init(wins: Int, losses: Int) {
+        self.wins = wins
+        self.losses = losses
+    }
+
+    static func == (lhs: Record, rhs: Record) -> Bool {
+        lhs.wins == rhs.wins && lhs.losses == rhs.losses
+    }
+}
+
+let recordA = Record(wins: 10, losses: 5)
+let recordB = Record(wins: 10, losses: 5)
+
+recordA == recordB /// Build error!
+
+
 //: [Next](@next)
