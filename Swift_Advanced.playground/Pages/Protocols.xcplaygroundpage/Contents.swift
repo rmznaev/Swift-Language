feb52d3b4672649a3ac93b80a63c54c86a50d9a0
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-02-04 18:16:31 +0100

    added optional protocol requirements code samples into the Protocols content

--- Swift_Advanced.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -429,4 +429,62 @@ for object in objects {
 /// Something that doesn't have an area
 
 
+// Optional Protocol Requirements
+@objc protocol CounterDataSource {
+    @objc optional func increment(forCount: Int) -> Int
+    @objc optional var fixedIncrement: Int { get }
+}
+
+class Counter {
+    var count = 0
+    var dataSource: CounterDataSource?
+
+    func increment() {
+        if let amount = dataSource?.increment?(forCount: count) {
+            count += amount
+        } else if let amount = dataSource?.fixedIncrement {
+            count += amount
+        }
+    }
+}
+
+class ThreeSource: NSObject, CounterDataSource {
+    let fixedIncrement = 3
+}
+
+var counter = Counter()
+counter.dataSource = ThreeSource()
+for _ in 1...4 {
+    counter.increment()
+    print(counter.count)
+}
+/// 3
+/// 6
+/// 9
+/// 12
+
+class TowardsZeroSource: NSObject, CounterDataSource {
+    func increment(forCount count: Int) -> Int {
+        if count == 0 {
+            return 0
+        } else if count < 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+}
+
+counter.count = -4
+counter.dataSource = TowardsZeroSource()
+for _ in 1...5 {
+    counter.increment()
+    print(counter.count)
+}
+/// -3
+/// -2
+/// -1
+/// 0
+/// 0
+
 //: [Next](@next)
