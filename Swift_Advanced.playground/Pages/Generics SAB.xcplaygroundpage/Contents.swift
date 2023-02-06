e42892257cc2f1cd9f74ebbba938f2378fdb6196
Ramazan Abdullayev <ramazan.aev@gmail.com>
2023-02-06 23:04:54 +0100

    added code samples for Generics

--- Swift_Advanced.playground/Pages/Generics SAB.xcplaygroundpage/Contents.swift
+++ Swift_Advanced.playground/Pages/Generics SAB.xcplaygroundpage/Contents.swift
@@ -23,5 +23,65 @@ enum EnumKeeperKind {
 }
 
 
+// Using type parameters
+protocol Pet {
+    var name: String { get }  /// all pets respond to a name
+}
+
+protocol Meowable {
+  func meow()
+}
+
+class Cat {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Dog {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+extension Cat: Pet {}
+extension Dog: Pet {}
+
+extension Cat: Meowable {
+    func meow() {
+        print("\(self.name) says meow!")
+    }
+}
+
+class Keeper<Animal: Pet> {
+    var name: String
+    var morningCare: Animal
+    var afternoonCare: Animal
+
+    init(
+        name: String,
+        morningCare: Animal,
+        afternoonCare: Animal
+    ) {
+        self.name = name
+        self.morningCare = morningCare
+        self.afternoonCare = afternoonCare
+    }
+}
+
+extension Array: Meowable where Element: Cat {
+    func meow() {
+        forEach { print("\($0.name) says meow!") }
+    }
+}
+
+let jason = Keeper(name: "Jason",
+                   morningCare: Cat(name: "Whiskers"),
+                   afternoonCare: Cat(name: "Sleepy"))
+
 
 //: [Next](@next)
